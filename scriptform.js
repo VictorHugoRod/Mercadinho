4886ea956397cf21574fb2a436bcc5fb7d3e5c10
faketorugo <hugorodriguesdeoliveiravictor@gmail.com>
2024-06-05 20:31:59 -0300

    terceiro commit

--- scriptform.js
+++ scriptform.js
@@ -3,7 +3,7 @@ function logar(){
     let senha = document.getElementById('senha').value;
 
     if(usuario == 'admin' && senha == 'admin'){
-        window.location.href = "";
+        window.location.href = "admin/catalogoadmin.html";
     }else if(usuario == 'admin' && senha != 'admin'){
         alert('Senha Incorreta');
     }else{
