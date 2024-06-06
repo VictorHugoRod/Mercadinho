e68be870fc54bf1e12a9fe221d8cbe64237bb006
Victor Hugo <hugorodriguesdeoliveiravictor@gmail.com>
2024-06-06 12:23:43 -0300

    quarto commit

---
+++ fechamentodepedido/scriptfechamento.js
@@ -0,0 +1,52 @@
+//FUNÇÃO PARA VOLTAR A PAGINA DE LOGIN----------------------------------------
+function voltar(){
+    window.location.href = "../../usuario/catalogousuario.html";
+}
+//----------------------------------------------------------------------------
+
+//FUNÇÃO PARA VERIFICAR PAGAMENTO---------------------------------------------
+function verificarpagamento(){
+    let formapagamento = document.getElementById('formapagamento').value;
+    let nome = document.getElementById('nome').value;
+    let cep = document.getElementById('cep').value;
+    let endereco = document.getElementById('endereco').value;
+    let numero = document.getElementById('numero').value;
+    let numerocartao = document.getElementById('numerocartao').value;
+    let cvc = document.getElementById('cvc').value;
+    let datavalidade = document.getElementById('datavalidade').value;
+
+    if(formapagamento == "pix"){
+        if(nome !="" && cep !="" && endereco !="" && numero !=""){
+            
+            if (confirm("O PAGAMENTO POR PIX DEVE SER REALIZADO ANTES DE CONFIRMAR O PEDIDO!!\n\nO PAGAMENTO FOI REALIZADO?")){
+               alert('PEDIDO CONFIRMADO!!');
+            } else {
+                alert('PREENCHA OS CAMPOS NOVAMENTE');
+            }
+        }
+    }else if(formapagamento == "cartao"){
+
+        if(nome !="" && cep !="" && endereco !="" && numero !="" && numerocartao !="" && cvc !="" && datavalidade !=""){
+            alert('PEDIDO CONFIRMADO!!');
+        }
+    }
+}
+//----------------------------------------------------------------------------
+
+//FUNÇÃO APARECER INFORMAÇÕES DE PAGAMENTO------------------------------------
+document.getElementById("formapagamento").addEventListener("change", function(){
+    var cartaoinfo = document.getElementById("cartaoinfo");
+    var pixinfo = document.getElementById("pixinfo");
+    
+    if(this.value === "cartao"){
+        cartaoinfo.style.display = "block";
+        pixinfo.style.display = "none";
+    }else if (this.value === "pix"){
+        cartaoinfo.style.display = "none";
+        pixinfo.style.display = "block";
+    }else{
+        cartaoinfo.style.display = "none";
+        pixinfo.style.display = "none";
+    }
+});
+//-----------------------------------------------------------------------------
--- scriptform.js
+++ scriptform.js
@@ -2,11 +2,13 @@ function logar(){
     let usuario = document.getElementById('usuario').value;
     let senha = document.getElementById('senha').value;
 
-    if(usuario == 'admin' && senha == 'admin'){
+    if(usuario == "admin" && senha == "admin"){
         window.location.href = "admin/catalogoadmin.html";
-    }else if(usuario == 'admin' && senha != 'admin'){
+    }else if(usuario == "admin" && senha != "admin"){
         alert('Senha Incorreta');
+    }else if(usuario == "" || senha == ""){
+        alert('Preencha os campos');
     }else{
-        window.location.href = ""
+        window.location.href = "usuario/catalogousuario.html";
     }
 }
